c72ca05719f17c8aa66612581405b1d96a459c8e
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-23 03:01:30 +0100

    onClick

--- coursera_app/src/App.js
+++ coursera_app/src/App.js
@@ -3,26 +3,58 @@ import Homepage from "./Homepage";
 import AboutLittleLemon from "./AboutLittleLemon";
 import Contact from "./Contact";
 import { Routes, Route, Link } from "react-router-dom";
-import logo192 from './assets/logo192.png'
-
-
+import logo192 from "./assets/logo192.png";
+//import ReactPlayer from "./react-player";
 
 function App() {
+  const bird1 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
+  );
+
+  const bird2 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
+  );
+
+  function toggle1() {
+    if (bird1.paused) {
+      bird1.play();
+    } else {
+      bird1.pause();
+    }
+  }
+
+  function toggle2() {
+    if (bird2.paused) {
+      bird2.play();
+    } else {
+      bird2.pause();
+    }
+  }
+
   return (
-    <div> 
-	  <nav>
-      <Link to="/" className="nav-item">Homepage</Link>
-      <Link to="/about" className="nav-item">About Little Lemon</Link>
-      <Link to="/contact" className="nav-item">Contact</Link>
-	  </nav>
-      <Routes> 
+    <div>
+      <nav>
+        <Link to="/" className="nav-item">
+          Homepage
+        </Link>
+        <Link to="/about" className="nav-item">
+          About Little Lemon
+        </Link>
+        <Link to="/contact" className="nav-item">
+          Contact
+        </Link>
+      </nav>
+      <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/about" element={<AboutLittleLemon />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
       <img src={logo192} alt="Logo192" />
+      <h1>React player</h1>
+      <button onClick={toggle1}>Caspian Tern 1</button>
+      <button onClick={toggle2}>Caspian Tern 2</button>
     </div>
   );
-};
+}
 
 export default App;
--- coursera_app/src/Contact.js
+++ coursera_app/src/Contact.js
@@ -2,6 +2,7 @@ function Contact() {
     return (
         <h1>Contact Little Lemon on this page</h1>
         
+        
     )
 }
 export default Contact;
